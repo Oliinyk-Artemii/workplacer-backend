c724319fe3610135734a462d2f39d90b5ae5c6ce
Artemii Oliinyk <aoliinyk@azercell.org>
2023-02-15 20:18:11 +0100

    [TECH] Add RegisterControllerAdvice.java

---
+++ src/main/java/oliin/apps/workplacer/rest/advice/wrapper/ErrorData.java
@@ -0,0 +1,11 @@
+package oliin.apps.workplacer.rest.advice.wrapper;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ErrorData {
+}
