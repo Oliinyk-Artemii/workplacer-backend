d6c08e493e8587e4b5e137f78ad282bb52c49960
Artemii Oliinyk <aoliinyk@azercell.org>
2023-05-03 01:37:21 -0400

    Update application.yml

--- src/main/java/oliin/apps/workplacer/domain/repository/SetupDataLoader.java
+++ src/main/java/oliin/apps/workplacer/domain/repository/SetupDataLoader.java
@@ -34,45 +34,45 @@ public class SetupDataLoader implements
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (alreadySetup) return;
-
-        Company company = Company.builder()
-                .name("Company name")
-                .isActive(true)
-                .build();
-
-        Office office = Office
-                .builder()
-                .name("Office name")
-                .isActive(true)
-                .build();
-
-        office.setCompany(company);
-
-        User officeManger = new User();
-        officeManger.setFirstName("Office");
-        officeManger.setLastName("Manager");
-        officeManger.setPassword(passwordEncoder.encode("test"));
-        officeManger.setEmail("manager");
-        officeManger.setRoles(Set.of(AuthorityType.OFFICE_MANAGER));
-        officeManger.setCompanies(new HashSet<>());
-
-        User employee = new User();
-        employee.setFirstName("Employee");
-        employee.setLastName("LoooongLastName");
-        employee.setPassword(passwordEncoder.encode("test"));
-        employee.setEmail("employee");
-        employee.setRoles(Set.of(AuthorityType.EMPLOYEE));
-        employee.setCompanies(new HashSet<>());
-
-        company.addUser(officeManger);
-        company.addUser(employee);
-        office.addUser(officeManger);
-        office.addUser(employee);
-
-        companyRepository.save(company);
-        officeRepository.save(office);
-
-        alreadySetup = true;
+//        if (alreadySetup) return;
+//
+//        Company company = Company.builder()
+//                .name("Company name")
+//                .isActive(true)
+//                .build();
+//
+//        Office office = Office
+//                .builder()
+//                .name("Office name")
+//                .isActive(true)
+//                .build();
+//
+//        office.setCompany(company);
+//
+//        User officeManger = new User();
+//        officeManger.setFirstName("Office");
+//        officeManger.setLastName("Manager");
+//        officeManger.setPassword(passwordEncoder.encode("test"));
+//        officeManger.setEmail("manager");
+//        officeManger.setRoles(Set.of(AuthorityType.OFFICE_MANAGER));
+//        officeManger.setCompanies(new HashSet<>());
+//
+//        User employee = new User();
+//        employee.setFirstName("Employee");
+//        employee.setLastName("LoooongLastName");
+//        employee.setPassword(passwordEncoder.encode("test"));
+//        employee.setEmail("employee");
+//        employee.setRoles(Set.of(AuthorityType.EMPLOYEE));
+//        employee.setCompanies(new HashSet<>());
+//
+//        company.addUser(officeManger);
+//        company.addUser(employee);
+//        office.addUser(officeManger);
+//        office.addUser(employee);
+//
+//        companyRepository.save(company);
+//        officeRepository.save(office);
+//
+//        alreadySetup = true;
     }
 }
