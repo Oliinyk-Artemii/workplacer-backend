81c044d9e10f8ac663d003773e1edaf8bfc62936
Artemii Oliinyk <aoliinyk@azercell.org>
2023-05-05 04:47:12 -0400

    Fix user role in user POST endpoint

--- src/main/java/oliin/apps/workplacer/feature/user/rest/mapper/CreateUserMapper.java
+++ src/main/java/oliin/apps/workplacer/feature/user/rest/mapper/CreateUserMapper.java
@@ -1,9 +1,9 @@
 package oliin.apps.workplacer.feature.user.rest.mapper;
 
 import oliin.apps.workplacer.domain.model.user.CreateUserRequest;
-import oliin.apps.workplacer.feature.user.rest.RegisterController;
-import oliin.apps.workplacer.feature.user.rest.CreateUserController;
 import oliin.apps.workplacer.feature.user.domain.model.AuthorityType;
+import oliin.apps.workplacer.feature.user.rest.CreateUserController;
+import oliin.apps.workplacer.feature.user.rest.RegisterController;
 import org.mapstruct.*;
 
 import java.util.Collections;
@@ -18,10 +18,15 @@ public interface CreateUserMapper {
     @Mapping(target = "authorities", expression = "java(Set.of(AuthorityType.OFFICE_MANAGER))")
     CreateUserRequest toCreateUserRequest(RegisterController.RegisterRequest request);
 
-    @Mapping(source="companyId",target = "companyIds", qualifiedByName = "idToIdsSet")
-    @Mapping(source="officeId",target = "officeIds", qualifiedByName = "idToIdsSet")
-    @Mapping(source="authorityType",target = "authorities", qualifiedByName = "idToIdsSet")
+    @Mapping(source = "companyId", target = "companyIds", qualifiedByName = "idToIdsSet")
+    @Mapping(source = "officeId", target = "officeIds", qualifiedByName = "idToIdsSet")
+    @Mapping(source = "authorityType", target = "authorities", qualifiedByName = "authorityToSet")
     CreateUserRequest toCreateUserRequest(CreateUserController.CreateUserRequest request);
+
+    @Named("authorityToSet")
+    public static Set<AuthorityType> authorityToSet(AuthorityType authorityType) {
+        return Set.of(authorityType);
+    }
 
     @Named("idToIdsSet")
     public static Set idToIdsSet(String id) {
